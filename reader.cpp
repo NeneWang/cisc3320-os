22c1e217079792b9a27c3178326ea6686c60a231
NelsonWang <wangnelson2@gmail.com>
2022-09-21 01:04:24 -0400

    reader

--- reader.cpp
+++ reader.cpp
@@ -3,19 +3,19 @@
 #include <string>
 
 int main () {
-std::ifstream myfile; 
-std::string myline;
-myfile.open("shopping_list.txt");
-if ( myfile.is_open() ) {
-while ( myfile ) { // equivalent to myfile.good()
-std::getline (myfile, myline);
-std::cout << myline << '\n';
-}
-}
-else {
-std::cout << "Couldn't open file\n";
-}
-return 0;  
+    std::ifstream myfile; 
+    std::string myline;
+    myfile.open("shopping_list.txt");
+    if ( myfile.is_open() ) {
+        while ( myfile ) { // equivalent to myfile.good()
+            std::getline (myfile, myline);
+            std::cout << myline << '\n';
+        }
+    }
+    else {
+        std::cout << "Couldn't open file\n";
+    }
+    return 0;  
 }
 
 
