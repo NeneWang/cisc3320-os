07885d7c337b5ad7025277dd7705547faa775a42
NelsonWang <wangnelson2@gmail.com>
2022-09-21 01:03:51 -0400

    reader

---
+++ reader.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main () {
+std::ifstream myfile; 
+std::string myline;
+myfile.open("shopping_list.txt");
+if ( myfile.is_open() ) {
+while ( myfile ) { // equivalent to myfile.good()
+std::getline (myfile, myline);
+std::cout << myline << '\n';
+}
+}
+else {
+std::cout << "Couldn't open file\n";
+}
+return 0;  
+}
+
+
