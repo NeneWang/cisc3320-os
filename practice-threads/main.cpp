63f3dc518da30a37db3ceea578f7d94024e328af
NelsonWang <wangnelson2@gmail.com>
2022-10-02 18:36:13 -0400

    bank example

--- practice-threads/main.cpp
+++ practice-threads/main.cpp
@@ -19,52 +19,63 @@ int GetRandom(int max){
     return rand() % max;
 }
 
-void ExecuteThread(int id){
-    // Get current time
-    auto nowTime = std::chrono::system_clock::now(); 
-    
-    // Convert to a time we can output
+std::string GetTime(){
+    auto nowTime = std::chrono::system_clock::now();
     std::time_t sleepTime = 
             std::chrono::system_clock::to_time_t(nowTime);
+    return std::ctime(&sleepTime);
+}
+
+
+double acctBalance = 100;
+
+std::mutex acctLock;
+ 
+void GetMoney(int id,
+        double withdrawal){
     
-    // Convert to current time zone
-    tm myLocalTime = *localtime(&sleepTime);
+    // The exception safe way to protect access
+    // to code within its scope. The lock is released 
+    // after execution leaves this scope
+    std::lock_guard<std::mutex> lock(acctLock);
     
-    // Print full time information
-    std::cout << "Thread " << id << 
-            " Sleep Time : " <<
-            std::ctime(&sleepTime) << "\n";
+    // Blocks access between lock and unlock
+    // until execution completes
+    // This isn't good to use however if an error 
+    // occurs between lock and unlock
+    // acctLock.lock();
     
-    // Get separate pieces
-    std::cout << "Month : " <<
-            myLocalTime.tm_mon << "\n";
-    std::cout << "Day : " <<
-            myLocalTime.tm_mday << "\n";
-    std::cout << "Year : " <<
-            myLocalTime.tm_year + 1900 << "\n";
-    std::cout << "Hours : " <<
-            myLocalTime.tm_hour << "\n";
-    std::cout << "Minutes : " <<
-            myLocalTime.tm_min << "\n";
-    std::cout << "Seconds : " <<
-            myLocalTime.tm_sec << "\n\n";
+    std::this_thread::sleep_for(std::chrono::seconds(3));
     
-    // Put the thread to sleep for up to 3 seconds
-    std::this_thread::sleep_for (std::chrono::seconds(GetRandom(3)));
-    nowTime = std::chrono::system_clock::now();
-    sleepTime = 
-            std::chrono::system_clock::to_time_t(nowTime);
-    std::cout << "Thread " << id << 
-            " Awake Time : " <<
-            std::ctime(&sleepTime) << "\n";
-    
+    std::cout << id << 
+            " tries to withdrawal $" <<
+            withdrawal << " on " <<
+            GetTime() << "\n";
+            
+    if((acctBalance - withdrawal) >= 0){
+        acctBalance -= withdrawal;
+        std::cout << "New Account Balance is $" <<
+                acctBalance << "\n";
+    } else {
+        std::cout << "Not Enough Money in Account\n";
+        std::cout << "Current Balance is $" <<
+                acctBalance << "\n";
+    }
+    // acctLock.unlock();
 }
 
 int main(){
-    std::thread th1 (ExecuteThread, 1);
-    th1.join();
-    std::thread th2 (ExecuteThread, 2);
-    th2.join();
+    std::thread threads[10];
+    
+    for(int i = 0; i < 10; ++i){
+        threads[i] = std::thread(GetMoney, i, 15);
+    }
+
+
+    for(int i = 0; i < 10; ++i){
+        threads[i].join();
+    }
+    
     return 0;
 }
 
