3b67ef38bdb18197440638f3be2c0cd36abecabe
NelsonWang <wangnelson2@gmail.com>
2022-10-13 21:31:30 -0400

    progress :construction:

--- hw2/main.cpp
+++ hw2/main.cpp
@@ -4,13 +4,17 @@
 
 #include <unistd.h>
 #include <stdlib.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main(){
-
+    time_t start, end;
     pid_t pid = fork();
+
     if(pid==0){
+        time(&start);
+        ios_base::sync_with_stdio(false);
         cout << "child "  << pid << endl;
         char* argument_list[] = {"ls", "-l", NULL}; 
         execvp("ls", argument_list);
@@ -18,6 +22,11 @@ int main(){
     }if(pid > 0){
         wait(0);
         cout << "Parent " << pid << endl;
+        time(&end);
+        double time_taken = double(end - start);
+        cout << "Time taken by program is : " << fixed
+            << time_taken << setprecision(5);
+        cout << " sec " << endl;
     }
     
     // std::cout << "testing gnu" << endl;
