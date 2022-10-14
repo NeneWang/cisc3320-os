2ce70672e57fea198edc5ad5dd6eac467471e80f
NelsonWang <wangnelson2@gmail.com>
2022-10-13 22:22:47 -0400

    progress :construction:

--- hw2/main.cpp
+++ hw2/main.cpp
@@ -20,18 +20,19 @@ int main(){
 
         execvp("./analyze.out", argument_list);
         exit(0);
+
     }if(pid > 0){
         wait(0);    
         int status;
         pid_t pid = 0;
         waitpid(pid, &status, 0);
         // pid_t wait(*status);    
-        cout << "Status: "<< status <<endl;
+        cout << "Exit value is: "<< status <<endl;
 
         auto stop = high_resolution_clock::now();
         auto duration = duration_cast<microseconds>(stop - start);
 
-        cout << "Time to finish all tasks: " << double(duration.count())/1000 << " milliseconds" << endl;
+        cout << "Time to finish all tasks: " << double(duration.count())/1000 << " ms" << endl;
     }
 }
 
