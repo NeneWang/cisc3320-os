be3940926f80bbf68aec100367ce2aeb72db688d
NelsonWang <wangnelson2@gmail.com>
2022-10-13 21:27:48 -0400

    progress :construction:

--- hw2/main.cpp
+++ hw2/main.cpp
@@ -1,13 +1,26 @@
 #include <stdio.h>
 #include <iostream>
 #include <sys/wait.h>
+
 #include <unistd.h>
+#include <stdlib.h>
 
 using namespace std;
 
 int main(){
-    fork();
-    std::cout << "testing gnu" << endl;
+
+    pid_t pid = fork();
+    if(pid==0){
+        cout << "child "  << pid << endl;
+        char* argument_list[] = {"ls", "-l", NULL}; 
+        execvp("ls", argument_list);
+    
+    }if(pid > 0){
+        wait(0);
+        cout << "Parent " << pid << endl;
+    }
+    
+    // std::cout << "testing gnu" << endl;
 
 }
 
