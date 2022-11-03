7b17ce9c497efbdb4c78b152dc133aa256f8ad4f
NelsonWang <wangnelson2@gmail.com>
2022-11-02 22:10:17 -0400

    progress :construction:

--- hw3/main.cpp
+++ hw3/main.cpp
@@ -1,29 +1,46 @@
 #include <iostream>
+#include <mutex>
+#include <thread>
+#include <cstdio>
 
 using namespace std;
 
+
+long sum = 0;
 void populateNumbers(int range, int *array){
     for(int i = 1; i<=range; i++){
         array[i-1] = i;
     }
 }
 
-long sumAll(int range, int arrayNums[]){
-    long sum = 0;
-    for(int i = 0; i < range; i++){
+void sumAll(int range, int start, int arrayNums[]){
+    
+    for(int i = start; i < range; i++){
         sum+=arrayNums[i];
     }
-
-    return sum;
 }
 
 int main(){
-    const int SIZE = 1000, SPLITS=5, FRAMESIZE=SIZE/(SPLITS-1);
+    const int SIZE = 1000, SPLITS=5, FRAMESIZE=SIZE/(SPLITS);
     int populatedArr[SIZE];
+    thread threads[SPLITS];
+
     populateNumbers(SIZE, populatedArr);
 
-    double sum = sumAll(FRAMESIZE, populatedArr); //This works
+
+    //  = sumAll(SIZE, SIZE-FRAMESIZE-1 ,populatedArr); //This works
+    for (int i = 0; i<SPLITS; i++){
+        int rangeMax = FRAMESIZE*(i+1), rangeMin = rangeMax-FRAMESIZE+1;
+        sumAll(rangeMax, rangeMin, populatedArr);
+        printf("\n%d %d\n", rangeMax, rangeMin);
+        // threads[i] = thread(sumAll, rangeMax, rangeMin, populatedArr);
+    }
+
+    // for (int i = 0; i<SPLITS; i++){
+    //     threads[i].join(); 
+    // }
     // double sum = sumAll(SIZE, populatedArr);
+    
     cout << populatedArr[4] <<  " " << sum << endl;
 }
 
