a1f35d0b93929eac690ee8bc130c61903da2d663
NelsonWang <wangnelson2@gmail.com>
2022-11-05 20:18:06 -0400

    progress :construction:

--- hw3/main.cpp
+++ hw3/main.cpp
@@ -2,49 +2,53 @@
 #include <mutex>
 #include <thread>
 #include <cstdio>
+#include <unistd.h>
 
 using namespace std;
 
-
 long sum = 0;
-void populateNumbers(int range, int *array){
-    for(int i = 1; i<=range; i++){
-        array[i-1] = i;
+void populateNumbers(int range, int *array)
+{
+    for (int i = 1; i <= range; i++)
+    {
+        array[i - 1] = i;
     }
 }
 
-void sumAll(int range, int start, int arrayNums[]){
-    
-    for(int i = start; i < range; i++){
-        sum+=arrayNums[i];
+void sumAll(int range, int start, int arrayNums[])
+{
+
+    for (int i = start; i < range; i++)
+    {
+        int temp = sum + arrayNums[i];
+        usleep(1);
+        sum = temp;
         // cout << "THREAD" << range<< endl;
     }
 }
 
-int main(){
-    const int SIZE = 1000, SPLITS=5, FRAMESIZE=SIZE/(SPLITS);
+int main()
+{
+    const int SIZE = 1000, SPLITS = 5, FRAMESIZE = SIZE / (SPLITS);
     int populatedArr[SIZE];
     thread threads[SPLITS];
 
     populateNumbers(SIZE, populatedArr);
 
-
     //  = sumAll(SIZE, SIZE-FRAMESIZE-1 ,populatedArr); //This works
-    for (int i = 0; i<SPLITS; i++){
-        int rangeMax = FRAMESIZE*(i+1), rangeMin = rangeMax-FRAMESIZE;
+    for (int i = 0; i < SPLITS; i++)
+    {
+        int rangeMax = FRAMESIZE * (i + 1), rangeMin = rangeMax - FRAMESIZE;
         // sumAll(rangeMax, rangeMin, populatedArr);
         // printf("\n%d %d\n", rangeMax, rangeMin);
         threads[i] = thread(sumAll, rangeMax, rangeMin, populatedArr);
     }
 
-    for (int i = 0; i<SPLITS; i++){
-        threads[i].join(); 
+    for (int i = 0; i < SPLITS; i++)
+    {
+        threads[i].join();
     }
     // double sum = sumAll(SIZE, populatedArr);
-    
-    cout << populatedArr[4] <<  " " << sum << endl;
+
+    cout << populatedArr[4] << " " << sum << endl;
 }
-
-
-
-
