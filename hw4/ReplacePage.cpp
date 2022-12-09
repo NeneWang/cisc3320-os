9a2a22b37efc687c62dd7622917186d3d97b80b8
NelsonWang <wangnelson2@gmail.com>
2022-12-08 20:59:08 -0500

    Committed by Maid :policewoman:

--- hw4/ReplacePage.cpp
+++ hw4/ReplacePage.cpp
@@ -8,9 +8,67 @@ using namespace std;
 
 class FIFO
 {
+    string reference;
+    int desired_frame = 3;
+    int length;
+    string frames="   ";
+    int faults = 0;
+
 public:
-    FIFO(string reference, int desired_frame)
+    FIFO(string reference, int desired_frame = 3, int refLen = 0)
     {
+        this->reference = reference;
+        this->desired_frame = desired_frame;
+        this->length = reference.size();
+        string spaces(this->length, ' ');
+        this->frames = spaces;
         
     }
-}
+
+    // Simulates iteration of the frames
+    void iterate()
+    {
+        int lastReferencedInPage = 0;
+        for (int i = 0; i < length; i++)
+        {
+            // Append but if over the desired frame then go back and edit the one on the pointer
+            char page = reference[i];
+
+            // Check if in the frames
+            if (frames.find(page) < frames.length())
+            {
+            }
+            else
+            {
+                cout << "Fault: "<< page << endl;
+                // If found but however is larger than index 3(2) then go reference to the first (0)
+                if (lastReferencedInPage >= this->desired_frame)
+                {
+                    lastReferencedInPage = 0;
+                }
+
+                // // If not found then add
+                // frames+=page;
+                frames[lastReferencedInPage] = page;
+                lastReferencedInPage++;
+                cout<<frames<<endl;
+                this->printFrames();
+            }
+            // this->printFrames();
+        }
+    }
+
+    void printFrames()
+    {
+        // Iterate pages in frame and print them
+
+        // for (char page : frames)
+        // {
+        //     cout << page << " ";
+        // }
+
+        cout << frames << endl;
+    }
+
+    // The idea is to keep a pointer of the last on the array
+};
--- hw4/test_unit.cpp
+++ hw4/test_unit.cpp
@@ -5,27 +5,30 @@
 #include <iostream>
 #include <vector>
 #include "TranslateAddr.cpp"
+#include "ReplacePage.cpp"
 
 using namespace std;
 
-
-void part_1(void){
+void part_1(void)
+{
     TranslateAddr ta("20221108");
     ta.computeAddressInformation();
 }
 
-void part_2(void){
+void part_2(void)
+{
     
-}
+    string reference = "70120304230321201701";
+    FIFO fifo(reference, 3, reference.size());
+    fifo.iterate();
+    
 
-void test_p4(void){
-    vector<int> res = p4(2); //Should return 2
-    TEST_ASSERT_(res.at(0) == 4, "Checking valueasd");
-    loop_vect(res);   
+
 }
 
 
 TEST_LIST = {
     // {"test_p4", test_p4},
-    {"part_1", part_1},
+    // {"part_1", part_1},
+    {"part_2", part_2},
     {0}};
